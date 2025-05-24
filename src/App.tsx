b0fd15d2ad7edce9de51a9d69d49473e8ab52549
Bereket <bereketdesalegn097@gmail.com>
2025-05-24 20:07:53 +0300

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 function App() {
   return (
     <div>
-      <Message />
-      <ListGroup />
-      <ListGroup />
+      <Alert>
+        Hello <span> World!</span>
+      </Alert>
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
 import { Fragment } from "react/jsx-runtime";
 import { useState } from "react";
 
-function ListGroup() {
-  const items: string[] = ["banana", "Apple", "Strawberry"];
+interface Props {
+  items: string[];
+  heading: string;
+  selectedItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, selectedItem }: Props) {
   //Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <Fragment>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
         {items.length === 0 && <p>No items found!</p>}
         {items.map((item, index) => (
@@ -19,7 +24,10 @@ function ListGroup() {
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              selectedItem(item);
+            }}
           >
             {item}
           </li>
